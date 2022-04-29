88afba00fb387978d9d5ca3356232a18797d9558
Pedro Nóbrega <pedrotuga3@gmail.com>
2022-04-29 17:37:32 +0100

    Changed to Intro to be showed on Game component

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,8 +1,13 @@
-export default function Trivia(){
+export default function Trivia({ startTrivia }) {
 	return (
 		<section>
-			
+			<center>
+				<img src="/images/giant.png" alt="" height="100px" />
+			</center>
+			<p className="intro_selection_subtitle">Do you know everything?</p>
+			<button className="intro_selection_btn" onClick={startTrivia}>
+				Start
+			</button>
 		</section>
-		
-	)
+	);
 }
