46fcc8f4a76cd6ad64f28a3385a8014d062c3c82
Pedro Nóbrega <pedrotuga3@gmail.com>
2022-05-02 18:17:49 +0100

    Added Scorebox and QuestionList Component

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Trivia from "./Trivia";
+import CategoriesLevel from "./CategoriesLevel";
+import QuestionList from "./QuestionList";
+import Scorebox from "./Scorebox";
 
 export default function Game() {
-	return <div>Game</div>;
+	const [gameStart, setGameStart] = useState(true);
+	const [customizingQuiz, setCustomizingQuiz] = useState(false);
+	const [urlApi, setUrlApi] = useState("");
+	const [customButton, setCustomButton] = useState({});
+
+	const startTrivia = () => {
+		setGameStart(false);
+		setCustomizingQuiz(true);
+	};
+
+	const getData = (data) => {
+		if (data.levels) {
+			let url = `http://162.19.27.138/api.php?level=${data.levels}&category=${data.categories}&amount=10`;
+			setUrlApi(url);
+			setCustomButton({});
+			setCustomizingQuiz(false);
+		} else {
+			setCustomButton({
+				animation: "wiggle 500ms",
+			});
+			setTimeout(() => setCustomButton({}), 500);
+		}
+	};
+
+	return (
+		<div className="game-container">
+			{gameStart ? (
+				<Trivia startTrivia={startTrivia} />
+			) : (
+				customizingQuiz && (
+					<CategoriesLevel getData={getData} customButton={customButton} />
+				)
+			)}
+			{!gameStart && !customizingQuiz && (
+				<div className="quiz-container">
+					<Scorebox />
+					<QuestionList />
+				</div>
+			)}
+		</div>
+	);
 }
