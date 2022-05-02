9d1be20dfcb42044acedb3addf20f44f9d5b110a
Pedro Nóbrega <pedrotuga3@gmail.com>
2022-05-02 18:01:45 +0100

    Changed paths of components

--- src/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function Home() {
-	return (
-		<section>
-			<center><img src="/images/giant.png" alt="" height="100px" /></center>
-			<p>dasd</p>
-		</section>
-	);
-}
